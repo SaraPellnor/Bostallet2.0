1f601c7199777c17190e32503552ed9d57470917
Sara Pellnor <sara.ahlquist@hotmail.com>
2025-05-31 09:52:23 +0200

    flyttar knapparna till vänster i start

--- app/Components/RegistryForm/RegistryForm.jsx
+++ app/Components/RegistryForm/RegistryForm.jsx
@@ -2,7 +2,7 @@
 import { redirect } from "next/navigation";
 const RegistryForm = () => {
   return (
-<div className="z-30 mb-10 flex flex-col items-center justify-center gap-2">
+<div className="z-30 w-full pl-5 mb-10 flex flex-col justify-center items-start gap-2">
            <button
           type="submit"
           className="shadow-md shadow-black transition bg-white duration-500 ease-out text-black py-3 px-10 rounded-full hover:scale-105"
