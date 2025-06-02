5729834b0b78496ecfa816f2b44a53a79d93b2d8
Sara Pellnor <sara.ahlquist@hotmail.com>
2025-06-02 22:50:38 +0200

    länkbyte för registerutdrag

--- app/Components/RegistryForm/RegistryForm.jsx
+++ app/Components/RegistryForm/RegistryForm.jsx
@@ -6,7 +6,7 @@ const RegistryForm = () => {
            <button
           type="submit"
           className="shadow-md shadow-black transition bg-white duration-500 ease-out text-black py-3 px-10 rounded-full hover:scale-105"
-                onClick={() => redirect("https://digitaliseringsinitiativet.se/belastningsregistret")}
+                onClick={() => redirect("https://l.facebook.com/l.php?u=https%3A%2F%2Fpolisen.se%2Ftjanster-tillstand%2Fbelastningsregistret%2Fbarn-annan-verksamhet%2F%3Ffbclid%3DIwZXh0bgNhZW0CMTAAYnJpZBEwYnpOUDE1eDc2SExITTFrVAEeG4XbsJK8trvk2wkLSQrwP1J2xO5xXFppYXiJV5UIT4PrGx-ShRvY9Mm3cCg_aem_IHBYhizoL5YLBpG-hN8B2Q&h=AT33qCTsrmAuxisMobFicrVznGJsozHofl4xFLTWDA9tDf3ZtqrewYSEWHbe8vz5YtZA0-ytXNopWou-2gaMWcQkWguGSbBxJKfjguEYtJjaiXaOGCvkZLNKdATNQ1pJGERcgY4pW9J6RRtpC5Nd")}
 
         >
           Belastningsutdrag
