afa948fec0f1581744f73daee2b03eaed9c25e77
Sara Pellnor <sara.ahlquist@hotmail.com>
2025-08-31 11:17:20 +0200

    fixad width på yttre elmenten html och body

--- app/Components/AdminPanel/AdminPanel.jsx
+++ app/Components/AdminPanel/AdminPanel.jsx
@@ -133,7 +133,7 @@ const AdminPanel = () => {
   ) : (
     <>
       <Header />
-      <div className=" flex flex-col justify-center gap-5 text-xl mt-1 pb-10">
+      <div className=" flex flex-col justify-center gap-5 text-xl mt-1 pb-10 w-full">
         <NewUser setIsAdmin={setIsAdminCheckBox} isAdmin={isAdminCheckBox} />
         <div className=" text-center pt-2 font-bold text-2xl">Medarbetare</div>
         <Search />
